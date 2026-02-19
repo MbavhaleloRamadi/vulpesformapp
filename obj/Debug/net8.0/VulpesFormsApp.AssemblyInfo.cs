4fc54f252b10c1827647855eea0d6fa6a9bfaede
Mbavhalelo Innocent Ramadi <mbavharamadi@gmail.com>
2026-02-19 08:45:37 +0200

    Attorneys Designs

--- obj/Debug/net8.0/VulpesFormsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/VulpesFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VulpesFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e783f4b320450ecb42da0f95a444d58ee2af1873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+270ae53d55b582a879bee96b7c372b116605da22")]
 [assembly: System.Reflection.AssemblyProductAttribute("VulpesFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VulpesFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
